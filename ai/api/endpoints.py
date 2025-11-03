31ce22f5f3b5cab48ada42c87eb30a7991084074
Owlumn <eduardermakov769@gmail.com>
2025-11-04 04:48:54 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        WIZION
+import os        Owlumn
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
