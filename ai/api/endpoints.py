d3b031df38022437c4e7791ca51347743805bfaa
WIZGEN <eduardermakov769@gmail.com>
2025-11-06 02:03:52 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        ORBCON
+import os        WIZGEN
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
