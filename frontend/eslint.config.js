23dc7fb47d0f3563e069a493d3779fb669e47fce
RADARE <eduardermakov769@gmail.com>
2025-11-21 22:43:11 +0800

    Update eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,4 +1,4 @@
-import js from '@eslint/js';   PAXINE
+import js from '@eslint/js';   RADARE
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks'; 
 import reactRefresh from 'eslint-plugin-react-refresh';
